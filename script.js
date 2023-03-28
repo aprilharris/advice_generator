b7c483749828c8bf008c83bcad715977f85bfe39
April Harris <54374224+aprilharris@users.noreply.github.com>
2023-03-28 00:02:16 +0000

    Initial commit

---
+++ script.js
@@ -0,0 +1,20 @@
+let request = new XMLHttpRequest();
+
+let url = "https://api.adviceslip.com/advice";
+
+request.open('GET', url, true);
+
+request.onload = function() {
+
+  let data=JSON.parse(this.response);
+   if(request.status >=200 && request.status < 400 ) {
+   
+ $('#advice').text(data["slip"].advice);
+}
+
+};
+
+request.send();
+
+
+
