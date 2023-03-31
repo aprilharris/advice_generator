73334253046ad021e3c4a69b52159d3dcb11f5b8
April Harris <54374224+aprilharris@users.noreply.github.com>
2023-03-31 16:49:49 +0000

    rewrote code so that it's triggered by an event handler (clicking the cookie trigger the event_ rather than when the page loads

--- script.js
+++ script.js
@@ -1,22 +1,24 @@
-let request = new XMLHttpRequest();
+//store cookie image in a variable
+const cookie = document.querySelector(".cookie");
 
-let url = "https://api.adviceslip.com/advice";
+// wrap all the JS code from version 1 in a function called generateAdvice
+function generateAdvice() {
+  let request = new XMLHttpRequest();
+  let url = "https://api.adviceslip.com/advice";
+  request.open("GET",url, true);
+  request.onload = function() {
+    let data = JSON.parse(this.response);
+    //confirm  API server is okay
+    if(request.status >=200 && request.status < 400) {
+    $("#advice").text(data.slip.advice);
+     
 
-request.open('GET', url, true);
-
-request.onload = function() {
-
-  let data=JSON.parse(this.response);
-  // confirm API server is okay
-   if(request.status >=200 && request.status < 400 ) {
-   
- // $('#advice').text(data["slip"].advice);
- $('#advice').text(data.slip.advice);
+  }
 }
 
-};
 
 request.send();
+};
 
 
-
+cookie.addEventListener("click", generateAdvice);
